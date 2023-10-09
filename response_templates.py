bcc3edd551365266f22c30a3000f4270c6fe0358
Anirudh Gajjala <anirudh.gajjala2000@gmail.com>
2023-10-08 23:41:06 -0400

    Adding response_templates.py

---
+++ response_templates.py
@@ -0,0 +1,14 @@
+response_templates = {
+
+    "investigator_pair_first_occurrence": "The investigator (or pair) first appears in Chapter {chapter} and Sentence {sentence}.",
+
+    "first_mention_of_crime": "The crime, which is of type {type_of_crime}, is first mentioned in Chapter {chapter}, Sentence {sentence}. Here are some details about it: {details}.",
+
+    "first_mention_of_perpetrator": "The perpetrator is first mentioned in Chapter {chapter}, Sentence {sentence}.",
+    
+    "three_words_around_perpetrator": "In Chapter {chapter}, Sentence {sentence}, the three words preceding perpetrator are: {three_preceding_words} and the three words following perpetrator are: {three_following_words}.",
+    
+    "detective_perpetrator_cooccurrence": "The detective and perpetrator appear together in Chapter {chapter}, Sentence {sentence}.",
+    
+    "other_suspects_first_introduction": "Other suspects are first introduced in Chapter {chapter}, Sentence {sentence}. Here are some details about them: {suspects}."
+}
